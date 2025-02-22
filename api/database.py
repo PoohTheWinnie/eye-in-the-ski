fd72ab5e3e8425085d8ce6a87e5082479fba9570
bondoo-winston <wcai.ctr@bondoo.ai>
2025-02-21 17:02:14 -0800

    Integrated supabase postgres instance

--- api/database.py
+++ api/database.py
@@ -1,10 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+from dotenv import load_dotenv
+import os
 
-SQL_ALCHEMY_DATABASE_URL = 'sqlite:///workout_app.db'
+load_dotenv()
 
-engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SQL_ALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
+
+"""
+Using NullPool in SQLAlchemy (poolclass=NullPool) is important in scenarios where you do not want SQLAlchemy to maintain a connection pool. 
+Instead, it creates and closes a new database connection for each request.
+"""
+
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL, client_encoding='utf8', poolclass=NullPool)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
